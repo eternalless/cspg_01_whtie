e1e66c9e0e224815cd61bde0cc1253df2db9568f
阿巨途 <tech@ajutu.com>
2018-11-13 16:09:42 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+<style lang="less">
+  @import "~@/assets/style/global";
 </style>
 <template>
   <div id="app">
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,27 +1,65 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
-  h1, h2 {
-    font-weight: normal;
+  .index {
+    font-size: 1.1rem;
+    .banner {
+      height: 20rem;
+      background-size: contain;
+      position: relative;
+      padding: 1rem;
+      .hello {
+      }
+      .flower-num {
+        position: absolute;
+        top: 1rem;
+        right: 1rem;
+        .iconfont {
+          font-size: 2rem;
+        }
+        .number {
+          display: inline-block;
+          position: relative;
+          color: #b33842;
+        }
+      }
+      .bg-hua {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+      }
+      .main-hua {
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        margin-left: -100px;
+      }
+
+    }
   }
 
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
 </style>
 
 <template>
   <div class="index">
+    <div class="banner">
+      <div class="hello mt5">
+        <p> 您好，张小二！</p>
+        <p class="mt5">您已经连续打卡25天</p>
+      </div>
 
+      <div class="flower-num">
+        <span class="iconfont icon-flower"></span>
+        <span class="number">20</span>
+        <span class="iconfont icon-lottery"></span>
+        <span class="number">5</span>
+      </div>
+      <img class="main-hua" src="../../static/img/s-2.png">
+      <img class="bg-hua" src="../../static/img/huayuan.png">
+    </div>
+    <div class="c">
+
+    </div>
   </div>
 </template>
 
